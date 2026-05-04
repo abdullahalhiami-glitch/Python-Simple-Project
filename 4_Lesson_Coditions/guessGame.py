3b24d7cb47e39f98ac38bf5e4ae535a73e4322e8
Abdullah Al-Hiami <abdullahalhiami@gmail.com>
2026-05-04 11:24:09 +0300

    new change

--- 4_Lesson_Coditions/guessGame.py
+++ 4_Lesson_Coditions/guessGame.py
@@ -12,16 +12,25 @@ while numberExit > 0:
     #then asked the user for a number that the user have guessed
     guessNumbwer = input("Guess a number between 1 to 10\nAnd the Enter the numer here: ")
     guessNumbwer = int(guessNumbwer)
-
     #then check if the number is equl or not
     if guessNumbwer == number:
         print("Congrates, You won.")
         break
     else:
-        print(f"Sorry, the Number you guessed is wrong\n The number is {number}")
-        print(f"Attempts Left {numberExit}")
-        print('------------------------------------------------')
-        numberExit -= 1
+        if guessNumbwer > number:
+            print(f"Sorry, the Number you guessed is wrong")
+            print("Your number is greater than the guessed number")
+            print(f"Attempts Left {numberExit}")
+            print('------------------------------------------------')
+            numberExit -= 1
+        else:
+            print(f"Sorry, the Number you guessed is wrong")
+            print("Your number is lower than the guessed number")
+            print(f"Attempts Left {numberExit}")
+            print('------------------------------------------------')
+            numberExit -= 1
+    if numberExit == 0:
+        print("------------ Your five attempts have finished. --------------")
 
 # #then asked the user for a number that the user have guessed
 # guessNumbwer = input("Guess a number between 1 to 100\nAnd the Enter the numer here: ")
