c37e54083a61d43b33929caaa7904ab993d20fbc
Abdullah Al-Hiami <abdullahalhiami@gmail.com>
2026-05-05 10:24:15 +0300

    continue the oop lesson

--- 14_Lesson_OOP/Class_Student.py
+++ 14_Lesson_OOP/Class_Student.py
@@ -1,10 +1,26 @@
 class Student:
+    @classmethod
+    def printClassStudentInfo(cls):
+        print("This Class For Register Student Information.")
+    name = "Abdullah"
     def __init__(self,name,age):
-        self.n = name
-        self.a = age
+        self.Sname = name
+        self.Sage = age
 
     def printStudent(self):
-        print(f"The student name is {self.n} and \n The student age is {self.a}")
+        print(f"The student name is {self.Sname} and \n The student age is {self.Sage}")
+
+class Course:
+    def __init__(self,Cname,Cap):
+        self.CourseName =  Cname
+        self.Capcity = Cap
+        self.StudentName = []
+    def showStudent(self):
+        print("---------------- Student ----------------")
+        for student in self.StudentName:
+            print(student)
+        print("-----------------------------------------")
+
 
 Student1 = Student("ِAbdullah", 45)
 Student2 = Student("ِAli", 98)
@@ -12,7 +28,9 @@ Student2 = Student("ِAli", 98)
 Student1.printStudent()
 Student2.printStudent()
 
-print(Student1.n)
-print(Student1.a)
+print(Student1.Sname)
+print(Student1.Sage)
+print(Student.name)
+print(Student.printClassStudentInfo())
 
         
